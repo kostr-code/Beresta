ce62089da23de038d1b3b16a7c12b02a71d6585b
DerFreimensch <62285401+DerFreimensch@users.noreply.github.com>
2023-01-07 00:25:24 +0300

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: F:/Programms/Beresta
-# Build directory: F:/Programms/Beresta/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
