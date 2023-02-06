106247790bfe7e8d13575978a684388bba027b27
Kostr <derfreimensch@yandex.ru>
2023-02-06 17:44:58 +0300

    добавлен core

--- App/include/window.h
+++ App/include/window.h
@@ -13,7 +13,7 @@
 #include <GLFW/glfw3.h> // Will drag system OpenGL headers
 
 
-#include "../include/menubar.h"
+#include "../include/core.h"
 
 
 static void glfw_error_callback(int error, const char *description);
